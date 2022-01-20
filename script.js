aa9766e8a0e365127ce63b5ef81272de2f754845
gassyrdaulet <97328371+gassyrdaulet@users.noreply.github.com>
2022-01-20 19:12:56 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,91 @@
-const gasButton = document.querySelector('#main .main .game-controls .gas-button');
-const wheels = document.querySelectorAll('.wheel');
+const btnPrev = document.querySelector('.prev');
+const btnNext = document.querySelector('.next');
+const track = document.querySelector('.image-track');
+const descWindow = document.querySelector('.desc-window');
+let position = 0;
+let descPos = 0;
+const slidesToShow = 1;
+const slider_window = document.querySelector('.image-window');
+let itemWidth = slider_window.clientWidth / slidesToShow;
+let itemWidthForDots = descWindow.clientWidth / slidesToShow;
+const slidesToScroll = 1;
+let itemsLeft;
+const allItems = document.querySelectorAll('.slider-item-image img');
+let itemsCount = allItems.length;
+const movePositionStep = itemWidth; 
+const movePositionStepForDesc = itemWidthForDots;
+const desc_items = document.querySelectorAll('.desc-item');
+const desc_track = document.querySelector('.desc-track');
+const allDots = document.querySelectorAll('.dots-item-text');
+let dot_position = 0;
+const any_item_except_ul = document.querySelectorAll('#main, #catalog, #delivery, #contacts, #reports, #footer');
 
-console.log("Wasup, Dog");
+allItems.forEach((item) => {
+    item.style.minWidth = `${itemWidth}px`;
+});
+desc_items.forEach((item) => {
+    item.style.minWidth = `${itemWidthForDots}px`;
+});
 
-wheels.forEach((item) => {
-	gasButton.addEventListener('click', () => {
-        gasButton.innerText = 'BREAK';
-        item.classList.toggle('active');
-        if(item.classList[1]!='active'){
-            gasButton.innerText = 'GAS';
-        }
+btnNext.addEventListener('click', () => {
+    itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;    
+    position = itemsLeft >= slidesToScroll ? position - movePositionStep : position - itemWidth * itemsLeft;
+    descPos = itemsLeft >= slidesToScroll ? descPos - movePositionStepForDesc : descPos - itemWidthForDots * itemsLeft; 
+    movePosition();
+    checkButtons();
+    dot_position++;
+    checkDotPosition();
+}); 
+btnPrev.addEventListener('click', () => {
+    itemsLeft = Math.abs(position) / itemWidth;
+    position = itemsLeft >= slidesToScroll ? position + movePositionStep : position + itemWidth * itemsLeft;
+    descPos = itemsLeft >= slidesToScroll ? descPos + movePositionStepForDesc : descPos + itemWidthForDots * itemsLeft; 
+    movePosition();
+    checkButtons();
+    dot_position--;
+    checkDotPosition();
+}); 
+
+const movePosition = () => {
+    track.style.transform = `translateX(${position}px)`;
+    desc_track.style.transform = `translateX(${descPos}px)`;
+};
+const checkButtons = () => {
+    btnPrev.disabled = position === 0;
+    btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
+};
+const checkDotPosition = () => {
+    for(a in allDots){
+        allDots[a].innerText = '○';
+    }
+    allDots[dot_position].innerText='◉';
+}
+
+checkDotPosition();
+checkButtons();
+
+const hamburger = document.querySelector('.header  .hamburger');
+const mobile_menu = document.querySelector('.header .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-list ul li a');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
 	});
 });
-gasButton.addEventListener('click', () => {
-    gasButton.classList.toggle('active');
+
+any_item_except_ul.forEach((item) => {
+	item.addEventListener('click', () => {
+        console.log(hamburger.classList[1]);
+        if(hamburger.classList[1] == 'active'){
+            hamburger.classList.toggle('active');
+            mobile_menu.classList.toggle('active');
+        }
+	});
 });
