517ba6a237e5d9465c36f4d1c791bba7db7e140f
gassyrdaulet <97328371+gassyrdaulet@users.noreply.github.com>
2022-01-24 03:32:21 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,91 +1,21 @@
-const btnPrev = document.querySelector('.prev');
-const btnNext = document.querySelector('.next');
-const track = document.querySelector('.image-track');
-const descWindow = document.querySelector('.desc-window');
-let position = 0;
-let descPos = 0;
-const slidesToShow = 1;
-const slider_window = document.querySelector('.image-window');
-let itemWidth = slider_window.clientWidth / slidesToShow;
-let itemWidthForDots = descWindow.clientWidth / slidesToShow;
-const slidesToScroll = 1;
-let itemsLeft;
-const allItems = document.querySelectorAll('.slider-item-image img');
-let itemsCount = allItems.length;
-const movePositionStep = itemWidth; 
-const movePositionStepForDesc = itemWidthForDots;
-const desc_items = document.querySelectorAll('.desc-item');
-const desc_track = document.querySelector('.desc-track');
-const allDots = document.querySelectorAll('.dots-item-text');
-let dot_position = 0;
-const any_item_except_ul = document.querySelectorAll('#main, #catalog, #delivery, #contacts-and-reports, #footer-section');
+const minutes = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");
+const deciSeconds = document.querySelector(".deci-seconds");
+let timerTime = 300000;
+let deciSecondG = 9;
 
-allItems.forEach((item) => {
-    item.style.minWidth = `${itemWidth}px`;
-});
-desc_items.forEach((item) => {
-    item.style.minWidth = `${itemWidthForDots}px`;
-});
-
-btnNext.addEventListener('click', () => {
-    itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;    
-    position = itemsLeft >= slidesToScroll ? position - movePositionStep : position - itemWidth * itemsLeft;
-    descPos = itemsLeft >= slidesToScroll ? descPos - movePositionStepForDesc : descPos - itemWidthForDots * itemsLeft; 
-    movePosition();
-    checkButtons();
-    dot_position++;
-    checkDotPosition();
-}); 
-btnPrev.addEventListener('click', () => {
-    itemsLeft = Math.abs(position) / itemWidth;
-    position = itemsLeft >= slidesToScroll ? position + movePositionStep : position + itemWidth * itemsLeft;
-    descPos = itemsLeft >= slidesToScroll ? descPos + movePositionStepForDesc : descPos + itemWidthForDots * itemsLeft; 
-    movePosition();
-    checkButtons();
-    dot_position--;
-    checkDotPosition();
-}); 
-
-const movePosition = () => {
-    track.style.transform = `translateX(${position}px)`;
-    desc_track.style.transform = `translateX(${descPos}px)`;
-};
-const checkButtons = () => {
-    btnPrev.disabled = position === 0;
-    btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
-};
-const checkDotPosition = () => {
-    for(a in allDots){
-        allDots[a].innerText = '○';
+let timerId = setInterval(() => {
+    minutes.innerText = parseInt(timerTime/60000);
+    seconds.innerText = parseInt((timerTime/1000)%60);
+    deciSeconds.innerText = deciSecondG;
+    timerTime = timerTime - 100;
+    deciSecondG = deciSecondG - 1;
+    if(deciSecondG == 0){
+        deciSecondG = 9;
     }
-    allDots[dot_position].innerText='◉';
-}
+    if(timerTime == 0){
+        clearInterval(timerId);
+        deciSeconds.innerText = 0;
+    }
+}, 100);
 
-checkDotPosition();
-checkButtons();
-
-const hamburger = document.querySelector('.header  .hamburger');
-const mobile_menu = document.querySelector('.header .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-list ul li a');
-
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
-});
-
-menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
-	});
-});
-
-any_item_except_ul.forEach((item) => {
-	item.addEventListener('click', () => {
-        console.log(hamburger.classList[1]);
-        if(hamburger.classList[1] == 'active'){
-            hamburger.classList.toggle('active');
-            mobile_menu.classList.toggle('active');
-        }
-	});
-});
